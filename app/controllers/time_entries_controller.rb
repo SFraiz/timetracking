51c7b34ac46e22d9d16bfad69263a4d30c47bf93
SFraiz <sergi.fraiz12@gmail.com>
2016-11-22 20:46:35 +0100

    Fourth week exercise

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,4 +1,5 @@
 class TimeEntriesController < ApplicationController
+	
 	def index
 		@project = Project.find(params[:project_id])
 		@entries = @project.time_entries.where('date > ?' , 1.month.ago.end_of_month)
